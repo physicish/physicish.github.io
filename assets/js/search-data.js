708abc06f8a5f485f89291ba70b16d9d74807686
physicish <ishmgupta@gmail.com>
2025-09-14 17:49:24 +0000

    Deploying to gh-pages from @ physicish/physicish.github.io@4accf5bf3c2c2a5b7201e0c517be08283c84ef8f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,15 +422,15 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-looking-for-remote-research-interns",
+          title: 'Looking for remote research interns!',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
